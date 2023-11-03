ebe7c8195fc3632f9d9067ce205a3db23a8d10fb
sashika20643 <sashikadulaj20643@gmail.com>
2023-11-03 11:26:22 +0530

    migration file added

---
+++ database/migrations/2023_11_03_054233_create_reservations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->date('checkin_date')->nullable();
+            $table->date('checkout_date')->nullable();
+            $table->string('Customer_name')->nullable();
+            $table->string('customer_id')->nullable();
+            $table->foreign('roomtype_id')->references('id')->on('roomtypess')->onDelete('cascade');
+            $table->foreign('room_id')->references('id')->on('rooms')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
